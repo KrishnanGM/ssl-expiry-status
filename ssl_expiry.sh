6dcfd1553eed1db4c6d1c1da87f4b67f1538da55
EC2 Default User <ec2-user@ip-172-31-46-103.ec2.internal>
2023-08-27 08:21:59 +0000

    Latest changes made in script

--- ssl_expiry.sh
+++ ssl_expiry.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-
-SLACK_WEBHOOK_URL="${SLACK_WEBHOOK_URL}"
-
-IFS=',' read -ra domains <<< "$DOMAINS"
-
-for domain in "${domains[@]}"; do
-    expiry_date=$(openssl s_client -connect "$domain":443 2>/dev/null | openssl x509 -noout -dates | grep 'notAfter' | cut -d '=' -f 2)
-    expiry_timestamp=$(date -d "$expiry_date" +%s)
-    current_timestamp=$(date +%s)
-    days_left=$(( ($expiry_timestamp - $current_timestamp) / 86400 ))
-
-    if [ "$days_left" -lt 365 ]; then
-        alert="SSL Expiry Alert\n   * Domain : $domain\n   * Warning : The SSL certificate for $domain will expire in $days_left days."
-        curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$alert\"}" "$SLACK_WEBHOOK_URL"
-    fi
-done
-
+set -x
+set -e
+check_ssl_expiry() {
+  domain="$1"
+  expiry_date=$(openssl s_client -servername "$domain" -connect "$domain":443 2>/dev/null | openssl x509 -noout -enddate | cut -d= -f2)
+  remaining_days=$(( ($(date -d "$expiry_date" +%s) - $(date +%s)) / 86400 ))
+  echo "Domain: $domain, Expiry Date: $expiry_date, Remaining Days: $remaining_days"
+  if [ "$remaining_days" -lt 365 ]; then
+    send_slack_alert "$domain" "$remaining_days"
+  fi
+}
+send_slack_alert() {
+  domain="$1"
+  remaining_days="$2"
+  message="SSL Expiry Alert\n   * Domain : $domain\n   * Warning : The SSL certificate for $domain will expire in $remaining_days days."
+  curl -X POST -H 'Content-type: application/json' --data "{
+    \"text\": \"$message\"
+  }" "$SLACK_WEBHOOK_URL"
+}
+while IFS= read -r domain || [[ -n "$domain" ]]; do
+  if [ -n "$domain" ]; then
+    echo "Processing domain: $domain"
+    check_ssl_expiry "$domain"
+  fi
+done < "$(dirname "$0")/domains.txt"
